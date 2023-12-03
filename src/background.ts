0f7d934f47ab525fd836010e74ac2c65fadcf2bf
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-12-03 22:32:54 +0200

    feat: add commands handler

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,32 @@
 chrome.runtime.onInstalled.addListener(async () => {
   let url = chrome.runtime.getURL('html/hello.html');
-  console.log('url in bgJS', url);
   chrome.tabs.create({ url });
+
+  chrome.storage.sync.get(['showClock'], result => {
+    console.log('result in bg', result);
+    if (result.showClock) {
+      chrome.action.setBadgeText({ text: 'On' });
+    }
+  });
+});
+
+chrome.commands.onCommand.addListener(command => {
+  chrome.tabs.update({}, async tab => {
+    debugger;
+    switch (command) {
+      case 'pin-current-tab':
+        chrome.tabs.update({ pinned: !tab?.pinned });
+        break;
+      case 'copy-current-tab':
+        chrome.tabs.duplicate(tab?.id!);
+        break;
+      case 'move-to-first':
+        chrome.tabs.move(tab?.id!, { index: 0 });
+        break;
+      case 'move-to-last':
+        const allTabs = await chrome.tabs.query({});
+        chrome.tabs.move(tab?.id!, { index: allTabs.length - 1 });
+        break;
+    }
+  });
 });
